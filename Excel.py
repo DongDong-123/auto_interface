93cb1cf02601789c4c079c47943e9e1d72110732
dongdong <zisehaiyang04@163.com>
2021-09-19 22:26:37 +0800

    添加公共函数

--- Excel.py
+++ Excel.py
@@ -5,8 +5,111 @@
 # @Software: PyCharm
 import xlrd
 import xlwt
-
+import os
 
 class ReadExcel:
+    """
+    读取excel
+    """
+    def __init__(self, path, sheet_name=None, sheet_index=None):
+        self.work_book = xlrd.open_workbook(path)
+        self.sheet = self.work_book.sheet_by_name(sheet_name)
+        self.sheet = self.work_book.sheet_by_index(sheet_index)
+        self.row = self.sheet.nrows  # 总行数
+        self.col = self.sheet.ncols  # 总列数
+        self.sheet_name = sheet_name
+
+    def get_all_sheel_names(self):
+        return self.work_book.sheet_names()
+
+    def get_sheel(self, work_book, name):
+        # 根据索引获取sheet，从0开始
+        sheet_content_by_index = work_book.sheet_by_index(0)
+
+        # 根据sheet名字获取sheet
+        sheet_content_by_name = work_book.sheet_by_name(name)
+        return sheet_content_by_index
+
+    def get_info_by_row(self):
+        """
+        逐行读取所有数据
+        :return: 生成器，一行数据组成的列表
+        """
+        for num in range(self.row):
+            yield list(map(self.process_float, self.sheet.row_values(num)))
+
+    def get_info_by_col(self):
+        """
+        逐列读取所有数据
+        :return: 生成器，一列数据组成的列表
+        """
+        for num in range(self.col):
+            yield list(map(self.process_float, self.sheet.col_values(num)))
+
+    def get_info_by_cell(self):
+        """
+        按单元格逐行读取数据，(可选择按列)
+        :return: 生成器  包含单元格内容、类型元组的列表
+        """
+        for row in range(1, self.row):
+            for col in range(self.col):
+                sheet_cell_value = self.sheet.cell(row, col).value
+                sheet_cell_value = list(map(self.process_float, [sheet_cell_value]))[0]
+                sheet_value_type = self.get_cell_type(row, col)
+                # sheet_cell_value = sheet_1.cell_value(row, col).encode('utf-8')
+                # sheet_cell_value = sheet_1.row(row)[col].value.encode('utf-8')
+                yield (sheet_cell_value, sheet_value_type)
+
+    def get_cell_type(self, row_index, col_index):
+        """
+        获取指定单元格内容的数据类型（ctype :  0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error）
+        :param row_index: int 行索引
+        :param col_index: int 列索引
+        :return: str 单元格的类型
+        """
+        sheet_value_type = self.sheet.cell(row_index, col_index).ctype
+        type_dict = {0: 'empty', 1: 'string', 2: 'number', 3: 'date', 4: 'boolean', 5: 'error'}
+        return type_dict[sheet_value_type]
+
+    def process_float(self, parm):
+        """
+        判断是否为float类型，且为整数，整数则转换为int，
+        :param parm: all type
+        :return: 原数据或转换为int类型的整数
+        """
+        if isinstance(parm, float) and parm == int(parm):
+            return int(parm)
+        else:
+            return parm
+
+
+class WriteExcel:
+    """
+    work_sheet.set_protect(1)
+    work_sheet.set_password('123456')  设置文档保护及密码，不允许修改（默认不保护）：
+    """
     def __init__(self):
-        pass
+        self.file_name = None
+
+    def write(self, datas, sheet_name, file_name):
+        # f = self.open_excel  # 合并excel代码
+        f = xlwt.Workbook()
+        sheet1 = f.add_sheet(sheet_name)
+        # sheet1.col(2).width = 256 * 40
+        # sheet1.col(3).width = 256 * 30
+        # sheet1.col(4).width = 256 * 30
+        # sheet1.col(5).width = 256 * 20
+        # sheet1.col(6).width = 256 * 20
+        # sheet1.col(7).width = 256 * 60
+        for i, e in enumerate(datas):
+            print(i, e)
+            for m, n in enumerate(e):
+                sheet1.write(i, m, n)
+
+        # sheet1.set_panes_frozen('1')
+        # sheet1.set_horz_split_pos(1)  # 水平冻结
+        # sheet1.set_vert_split_pos(1)  # 垂直冻结
+        # sheet1.set_show_headers(0)  # 设置隐藏行、列标签（默认为显示）
+        if not os.path.exists(os.path.join(os.getcwd(), 'tmp')):
+            os.mkdir(os.path.join(os.getcwd(), 'tmp'))
+        f.save(r"tmp\{}.xls".format(file_name))
--- exe_sql.py
+++ exe_sql.py
@@ -4,3 +4,35 @@
 # @FileName: exe_sql.py
 # @Software: PyCharm
 import pymysql
+import time
+from readconfig import ReadMySqlConfig
+import os
+
+
+class ExeSql:
+    def __init__(self):
+        self.host = ReadMySqlConfig().host()
+        self.user = ReadMySqlConfig().user()
+        self.passwd = ReadMySqlConfig().passwd()
+        self.port = int(ReadMySqlConfig().port())
+        self.db = ReadMySqlConfig().db()
+        self.day = time.strftime('%Y-%m-%d', time.localtime())
+
+    def exe_sql(self, sql):
+        conn = pymysql.connect(host=self.host, user=self.user, password=self.passwd, port=self.port, db=self.db,
+                                charset="utf8")
+        cur = conn.cursor()
+        time_now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+        cur.execute(sql)
+        conn.commit()
+        # print("{} 已执行".format(sql))
+        res = cur.fetchall()
+        if not os.path.exists(os.path.join(os.getcwd(), 'logs')):
+            os.mkdir(os.path.join(os.getcwd(), 'logs'))
+        with open('logs\{}.log'.format(self.day), '+a', encoding='utf-8') as f:
+            f.write('{}  {}  execute sql {}'.format(time_now, self.db, sql) + '\n')
+            f.write('{}  =={} result== '.format(time_now, self.db) + str(res) + '\n')
+        cur.close()
+        conn.close()
+        return res
